8029322d9e3148b57dd293a7d32dc9851b165c70
Marian <marian@becher-home.net>
2022-11-04 21:31:37 +0100

    Unity Version

--- Assets/Level/Level.cs
+++ Assets/Level/Level.cs
@@ -5,4 +5,7 @@ public class Level
     public Tile[,] tiles;
     public Vector2Int spawnDark;
     public Vector2Int spawnLight;
+
+    public int Width => tiles.GetLength(0);
+    public int Height => tiles.GetLength(1);
 }
--- Assets/Level/LevelManager.cs
+++ Assets/Level/LevelManager.cs
@@ -6,6 +6,8 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject lightBoxPrefab;
     [SerializeField] private GameObject darkBoxPrefab;
 
+    public Level level;
+
     private void Awake()
     {
         LoadLevel("test.txt");
@@ -13,6 +15,20 @@ public class LevelManager : MonoBehaviour
 
     public void LoadLevel(string path)
     {
-         LevelUtils.LoadLevelDataFromFile(path);
+        level = LevelUtils.LoadLevelDataFromFile(path);
+        for(int x = 0; x < level.Width; x++)
+        {
+            for(int y=0; y < level.Height; y++)
+            {
+                if(level.tiles[x,y].type == Tile.Type.Dark)
+                {
+
+                }
+                else if(level.tiles[x,y].type == Tile.Type.Light)
+                {
+
+                }
+            }
+        }
     }
 }
