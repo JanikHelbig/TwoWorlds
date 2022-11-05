03bc0aeb50bdb68b07e3bcf96ef309353513ebac
Janik Helbig <helbigjanik@gmail.com>
2022-11-05 16:32:30 +0100

    Remove garbage and make gameplay

---
+++ Assets/Utility/DirectionUtility.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Mathematics;
+
+namespace Utility
+{
+    public class DirectionUtility
+    {
+        public static Direction From(float2 v)
+        {
+            float2 abs = math.abs(v);
+
+            return abs.x > abs.y
+                ? v.x > 0
+                    ? Direction.EAST
+                    : Direction.WEST
+                : v.y > 0
+                    ? Direction.NORTH
+                    : Direction.SOUTH;
+        }
+    }
+}
