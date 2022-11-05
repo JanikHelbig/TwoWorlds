eb69d4a22813a99ab29e79b941a0ba07a43d9a6b
Marian <marian@becher-home.net>
2022-11-05 19:52:59 +0100

    Added character model

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -16,6 +16,8 @@ namespace Character
         [Header("Settings")]
         [SerializeField] private World world;
 
+        private Animator _animator;
+
         private Transform _tf;
 
         private float _moveDelay = 0;
@@ -23,6 +25,7 @@ namespace Character
         private void Awake()
         {
             _tf = transform;
+            _animator = GetComponentInChildren<Animator>();
         }
 
 
@@ -48,12 +51,29 @@ namespace Character
             if (levelManager.IsOccuipiedByOtherPlayer(this.gameObject, nextPos))
                 return;
 
+            switch(d)
+            {
+                case Direction.NORTH:
+                    this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+                case Direction.EAST:
+                    this.transform.rotation = Quaternion.Euler(0, 90, 0);
+                    break;
+                case Direction.WEST:
+                    this.transform.rotation = Quaternion.Euler(0, 270, 0);
+                    break;
+                case Direction.SOUTH:
+                    this.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    break;
+            }
+
             Tile t = levelManager.level[nextPos.x, nextPos.y];
             if(t.IsWalkable() && t.world == this.world)
             {
                 //TODO move animatin
+                _animator.SetBool("Move", true);
                 Vector3 target = _tf.localPosition.With(x: nextPos.x, z: nextPos.y);
-                this.transform.DOLocalMove(target, 0.5f).Play();
+                this.transform.DOLocalMove(target, 0.5f).Play().OnComplete(()=>_animator.SetBool("Move",false));
                 _moveDelay = 0.5f;
             }
             if(t.IsPushable() && t.world != this.world && levelManager.level.CanMoveTile(nextPos, d))
@@ -61,7 +81,8 @@ namespace Character
                 //TODO push move animatin
                 levelManager.level.TryMoveTile(nextPos, d);
                 Vector3 target = _tf.localPosition.With(x: nextPos.x, z: nextPos.y);
-                this.transform.DOLocalMove(target, 0.5f).Play();
+                _animator.SetBool("Push", true);
+                this.transform.DOLocalMove(target, 0.5f).Play().OnComplete(() => _animator.SetBool("Push", false));
                 _moveDelay = 0.5f;
             }
         }
