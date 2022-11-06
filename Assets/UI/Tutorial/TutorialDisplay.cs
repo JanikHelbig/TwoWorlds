cdff33c304cbfd66e1618e79f04db10a5895b2ba
Marian <marian@becher-home.net>
2022-11-06 17:11:22 +0100

    rest for first view levels

--- Assets/UI/Tutorial/TutorialDisplay.cs
+++ Assets/UI/Tutorial/TutorialDisplay.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TutorialDisplay : MonoBehaviour
 {
     public int targetLevel;
     private LevelManager manager;
+    private SpriteRenderer[] sprites;
     [SerializeField] AudioSource souce;
 
 
     private void Awake()
     {
+        bool visible = false; ;
+        manager = FindObjectOfType<LevelManager>();
+        sprites = GetComponentsInChildren<SpriteRenderer>();
+        foreach (var s in sprites)
+            s.color = new Color(1, 1, 1, 0);
 
-        manager = FindObjectOfType<LevelManager>(); 
         manager.OnLevelCompleted += () => {
-            this.gameObject.SetActive(false);
+            if (!visible)
+                return;
+            foreach (var s in sprites)
+                s.DOFade(0, 0.3f).Play();
         };
+
         manager.OnLevelLoaded += ()=> {
-            this.gameObject.SetActive(manager.CurrentLevel == targetLevel && !manager.IsCustomLevel);
             if (souce && manager.CurrentLevel == targetLevel && !manager.IsCustomLevel)
-                souce.Play();
+            {
+                foreach (var s in sprites)
+                    s.DOFade(1, 0.3f).Play();
+
+                if (!souce.isPlaying)
+                    souce.Play();
+                visible = true;
+            }
         };
     }
 }
